375fa7a5bc3cf0fd21d769bea2685c25e8e8530a
William Zhang <willzhang73@gmail.com>
2024-01-31 18:54:31 +0000

    typescript-functions updated

--- typescript-functions/main.ts
+++ typescript-functions/main.ts
@@ -11,7 +11,7 @@ function greet(name: string): string {
 console.log(greet('Beavis'));
 console.log(greet('SpongeBob'));
 
-const getArea: (width: number, height: number) => number = (width, height) => {
+const getArea = (width: number, height: number): number => {
   return width * height;
 };
 
@@ -30,14 +30,14 @@ const person2: Person = {
   firstName: 'John',
   lastName: 'Henry',
 };
-const getFirstName: (person: Person) => string = (person) => {
+const getFirstName = (person: Person): string => {
   return person.firstName;
 };
 
 console.log('the firstName of person1: ', getFirstName(person1));
 console.log('the firstName of person2: ', getFirstName(person2));
 
-const getLastElement: (array: any[]) => any = (array) => {
+const getLastElement = (array: any[]): unknown => {
   return array.at(-1);
 };
 
@@ -46,10 +46,7 @@ const array2: boolean[] = [true, true, false, true];
 console.log('the last element of array1: ', getLastElement(array1));
 console.log('the last element of array1: ', getLastElement(array2));
 
-const callOtherFunction: (otherFunction: any, params: unknown) => any = (
-  otherFunction,
-  params
-) => {
+const callOtherFunction = (otherFunction: any, params: unknown): unknown => {
   return otherFunction(params);
 };
 
