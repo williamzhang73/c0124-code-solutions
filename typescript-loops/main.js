2433dd322a0d9cc0220719010b264ab4afa56bd7
William Zhang <willzhang73@gmail.com>
2024-02-01 23:39:14 +0000

    typescript-loops updated again

--- typescript-loops/main.js
+++ typescript-loops/main.js
@@ -16,7 +16,6 @@ function getNumbersToTen() {
   }
   return numbers;
 }
-console.log('value of getNumbersToTen is: ', getNumbersToTen());
 function getEvenNumbersToTwenty() {
   const evenNumbers = [];
   let currentNumber = 2;
@@ -26,7 +25,6 @@ function getEvenNumbersToTwenty() {
   }
   return evenNumbers;
 }
-console.log(getEvenNumbersToTwenty());
 function repeatWord(word, times) {
   let count = 1;
   let repeated = '';
@@ -36,13 +34,11 @@ function repeatWord(word, times) {
   }
   return repeated;
 }
-console.log('value of repeatWord: ', repeatWord('word', 3));
 function logEachCharacter(string) {
   for (let i = 0; i < string.length; i++) {
     console.log(`value at index ${i}: `, string[i]);
   }
 }
-logEachCharacter('Hello World');
 function doubleAll(numbers) {
   const doubled = [];
   for (let i = 0; i < numbers.length; i++) {
@@ -50,15 +46,10 @@ function doubleAll(numbers) {
   }
   return doubled;
 }
-console.log('value of doubleAll: ', doubleAll([1, 2, 3]));
 /* interface Object {
   firstName: 'William';
   lastName: 'Zhang';
 } */
-const person = {
-  firstName: 'William',
-  lastName: 'Zhang',
-};
 function getKeys(object) {
   const keys = [];
   for (const key in object) {
@@ -66,7 +57,6 @@ function getKeys(object) {
   }
   return keys;
 }
-console.log('value of getKeys: ', getKeys(person));
 function getValues(object) {
   const values = [];
   for (const key in object) {
@@ -74,4 +64,3 @@ function getValues(object) {
   }
   return values;
 }
-console.log('value of getValues: ', getValues(person));
--- typescript-loops/main.ts
+++ typescript-loops/main.ts
@@ -15,7 +15,6 @@ function getNumbersToTen(): number[] {
   }
   return numbers;
 }
-console.log('value of getNumbersToTen is: ', getNumbersToTen());
 
 function getEvenNumbersToTwenty(): number[] {
   const evenNumbers = [];
@@ -26,7 +25,6 @@ function getEvenNumbersToTwenty(): number[] {
   }
   return evenNumbers;
 }
-console.log(getEvenNumbersToTwenty());
 
 function repeatWord(word: string, times: number): string {
   let count = 1;
@@ -38,15 +36,12 @@ function repeatWord(word: string, times: number): string {
 
   return repeated;
 }
-console.log('value of repeatWord: ', repeatWord('word', 3));
 
 function logEachCharacter(string: string): void {
   for (let i = 0; i < string.length; i++) {
     console.log(`value at index ${i}: `, string[i]);
   }
 }
-
-logEachCharacter('Hello World');
 
 function doubleAll(numbers: number[]): number[] {
   const doubled = [];
@@ -55,16 +50,12 @@ function doubleAll(numbers: number[]): number[] {
   }
   return doubled;
 }
-console.log('value of doubleAll: ', doubleAll([1, 2, 3]));
 
 /* interface Object {
   firstName: 'William';
   lastName: 'Zhang';
 } */
-const person = {
-  firstName: 'William',
-  lastName: 'Zhang',
-};
+
 function getKeys(object: Record<string, unknown>): string[] {
   const keys = [];
   for (const key in object) {
@@ -73,8 +64,6 @@ function getKeys(object: Record<string, unknown>): string[] {
   return keys;
 }
 
-console.log('value of getKeys: ', getKeys(person));
-
 function getValues(object: Record<string, unknown>): any {
   const values = [];
   for (const key in object) {
@@ -82,5 +71,3 @@ function getValues(object: Record<string, unknown>): any {
   }
   return values;
 }
-
-console.log('value of getValues: ', getValues(person));
