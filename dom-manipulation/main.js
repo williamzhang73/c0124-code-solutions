9b1181ea229e3a95125737d3b477fdc65d3eb72d
William Zhang <willzhang73@gmail.com>
2024-02-12 23:21:21 +0000

    dom-manipulation created

---
+++ dom-manipulation/main.js
@@ -0,0 +1,28 @@
+'use strict';
+const $buttonClick = document.querySelector('button');
+const $clickCount = document.querySelector('.click-count');
+console.log($clickCount);
+console.log('textContent: ', $clickCount?.textContent);
+if (!$buttonClick) {
+  throw new Error('the $buttonClick query failed');
+} else if (!$clickCount) {
+  throw new Error('the $clickCount query failed');
+}
+let clickCount = 0;
+$buttonClick?.addEventListener('click', (event) => {
+  clickCount++;
+  if (clickCount === 4) {
+    $buttonClick.classList.remove('cold');
+    $buttonClick.classList.add('warm');
+  } else if (clickCount === 8) {
+    $buttonClick.classList.remove('warm');
+    $buttonClick.classList.add('hot');
+  } else if (clickCount === 12) {
+    $buttonClick.classList.remove('hot');
+    $buttonClick.classList.add('nuclear');
+  } else if (clickCount === 16) {
+    $buttonClick.classList.remove('nuclear');
+    $buttonClick.classList.add('tepid');
+  }
+  $clickCount.textContent = `Clicks: ${clickCount}`;
+});
