474d6dc89cc8d776a038f07aa21023b9b2f52e80
William Zhang <willzhang73@gmail.com>
2024-02-12 18:25:15 +0000

    dom-query created

---
+++ dom-querying/main.js
@@ -0,0 +1,15 @@
+'use strict';
+console.log('hello, world');
+const $h1 = document.querySelector('h1');
+console.log('$h1: ', $h1);
+console.dir($h1);
+const $explanationID = document.querySelector('#explanation');
+console.log('$explanationID: ', $explanationID);
+console.dir($explanationID);
+const $hintClass = document.querySelector('.hint');
+console.log('$hintClass: ', $hintClass);
+console.dir($hintClass);
+const $pAll = document.querySelectorAll('p');
+console.log('$pAll: ', $pAll);
+const $exampleClassLink = document.querySelectorAll('.example-link');
+console.log('$exampleClassLink: ', $exampleClassLink);
