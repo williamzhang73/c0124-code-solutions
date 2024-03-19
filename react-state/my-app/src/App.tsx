cd114b03c1a578b105965c4485bba8d51cd7db3c
William Zhang <willzhang73@gmail.com>
2024-03-19 20:35:42 +0000

    images decription updated

--- react-state/my-app/src/App.tsx
+++ react-state/my-app/src/App.tsx
@@ -9,8 +9,8 @@ const headerText = 'React Image Bank';
 const imageSrc = ['/monkey.png', '/tiger.png'];
 const imageCap = ['This Is a Monkey', 'This Is a Tiger'];
 const imageDescrip = [
-  `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam incidunt sit dolorum accusantium sapiente reiciendis maxime dolores ullam delectus, commodi placeat eveniet, quam voluptates facere et magnam architecto quaerat velit voluptas rerum quos asperiores quis in ! Nemo aut aliquam quas dolore quaerat, error numquam odio modi eos vero. Modi nisi eos autem ? Iusto hic impedit aspernatur labore quos nesciunt ? Ad dolore pariatur aperiam nemo, maiores laboriosam quam quod nisi ipsum, dicta sunt laborum similique repellat ?`,
-  `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam incidunt sit dolorum accusantium sapiente reiciendis maxime dolores ullam delectus, commodi placeat eveniet, quam voluptates facere et magnam architecto quaerat velit voluptas rerum quos asperiores quis in ! Nemo aut aliquam quas dolore quaerat, error numquam odio modi eos vero. Modi nisi eos autem ? Iusto hic impedit aspernatur labore quos nesciunt ? Ad dolore pariatur aperiam nemo, maiores laboriosam quam quod nisi ipsum, dicta sunt laborum similique repellat ?`,
+  `This image is about Monkey`,
+  `This image is about Tiger`,
 ];
 
 const buttonText = 'Click for Next Image';
