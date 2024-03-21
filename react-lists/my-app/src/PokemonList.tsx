478dffef5f931b411109e63aa1f4856018921ddf
William Zhang <willzhang73@gmail.com>
2024-03-21 22:09:15 +0000

    updated

--- react-lists/my-app/src/PokemonList.tsx
+++ react-lists/my-app/src/PokemonList.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type Entry = {
   number: string;
   name: string;
@@ -5,9 +7,19 @@ type Entry = {
 type Props = {
   pokedex: Entry[];
 };
+
 export function PokemonList({ pokedex }: Props) {
+  const [isSelected, setIsSelected] = useState(false);
+  function handleListClick() {
+    /*     console.log(item.name); */
+    setIsSelected(true);
+  }
+  const backgroundColor = isSelected ? 'green' : 'black';
   const li = pokedex.map((item) => (
-    <li key={item.number} style={{ textAlign: 'left' }}>
+    <li
+      key={item.number}
+      style={{ textAlign: 'left', backgroundColor: backgroundColor }}
+      onClick={handleListClick}>
       {item.name}
     </li>
   ));
