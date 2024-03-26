3b1a82d16e873fac8a12a5644ccbc70611a2ea7d
William Zhang <willzhang73@gmail.com>
2024-03-26 17:39:33 +0000

    react-effects created

---
+++ react-effects/my-app/src/List.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove me */
+import { useEffect, useState } from 'react';
+import { readItems, type Item } from './read';
+
+export function List() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState<Item[]>([]);
+  const [error, setError] = useState<unknown>();
+  useEffect(() => {
+    async function work() {
+      try {
+        const response = await readItems();
+        setItems(response);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    }
+    work();
+  }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error! {error instanceof Error ? error.message : 'Unknown Error'}
+      </div>
+    );
+  }
+
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          {item.id}: {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
